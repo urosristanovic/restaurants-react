86f095613586b5421c8b5008a25e9263e1dc4242
Uros Ristanovic <urosristanovic@gmail.com>
2021-11-02 15:37:08 +0100

    Implemented price button using map with data from json

--- src/components/Price/buttons.jsx
+++ src/components/Price/buttons.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import PriceRanges from '../../assets/json/prices.json';
 
 const Buttons = () => {
-  return <div className='btns' id='btns-price'></div>;
+  return (
+    <div className='btns' id='btns-price'>
+      {PriceRanges.map(price => {
+        return (
+          <button key={price.id} className={price.label} value={price.label}>
+            <span className='tooltiptext green'>
+              {price.minAvgPricePerMeal}-{price.maxAvgPricePerMeal}
+            </span>
+            {price.note}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Buttons;
